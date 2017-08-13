765b4f3f712450aa536bd23ddae5dceb49412df6
Adam Roach <adamroach504@gmail.com>
2017-08-13 18:19:46 -0500

    #comment refactored code to be cleaner, additional flagging, added comments

---
+++ MinerKiller/MinerKiller.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text.RegularExpressions;
+
+namespace MinerKiller
+{
+    class MinerKiller
+    {
+        private int[] _PortList = new[]
+        {
+            9999,
+            14444,
+            14433,
+            6666,
+            16666,
+            6633,
+            16633,
+            4444,
+            14444,
+            3333,
+            13333,
+            7777,
+            5555
+        };
+
+        private string[] _Nvidia = new[]
+        {
+            "nvcompiler.dll",
+            "nvopencl.dll",
+            "nvfatbinaryLoader.dll",
+            "nvapi64.dll",
+            "OpenCL.dll"
+        };
+
+        private string[] _Amd = new[] { "" };
+
+        
+        //todo: amd libraries
+        public MinerKiller()
+        {
+            
+
+        }
+
+        public void Scan()
+        {
+            // Get processes with active TCP connections
+            var cons = GetConnections();
+
+            foreach (Process p in GetProcesses())
+            {
+                int level = 0; // Number to describe the likelihood a process being a miner.
+                int modCount = 0;
+                foreach (ProcessModule pMod in p.Modules)
+                {
+                    foreach (string name in _Nvidia)
+                        if (pMod.ModuleName.ToLower().Equals(name.ToLower()))
+                            modCount++;
+                }
+
+
+                // If process has loaded more than 2 GPU libraries, increave level
+                if (modCount > 2)
+                {
+                    Logger.LogWarn("Potential Miner Found - Process: " + p.ProcessName + ".exe, ProcessId: " + p.Id);
+                    level += 1;
+                }
+                
+                // Check tcp connections associated with suspect process
+                var tiedConnections = cons.Where(x => x.ProcessId == p.Id);
+                var badPorts = tiedConnections.Where(x => _PortList.Any(y => y == x.RemotePort));
+                foreach (var conn in badPorts)
+                {
+                    Logger.Log("\t" + conn);
+                    level += 1;
+                }
+
+
+                // Check commandline arguments of suspect process
+                var args = GetCommandLine(p);
+
+                // If commandline contains active tcp port, increase level by 2
+                foreach(var conn in badPorts)
+                {
+                    if (args.Contains(conn.RemotePort.ToString()))
+                    {
+                        level += 2;
+                        Logger.Log("\tActive TCP Port in CMD Args: " + conn.RemotePort);
+                    }
+                        
+                }
+
+
+                
+                if (level >= 2)
+                {
+                    Logger.Log("\tProcess Warning Level: " + level);
+                    try
+                    {
+                        p.Kill();
+                        Logger.LogSuccess("Successfully killed Silent Miner: " + p.ProcessName + ".exe!");
+                    }
+                    catch (Exception e)
+                    {
+                        Logger.LogError("Failed to kill Silent Miner: " + p.ProcessName + ".exe!\r\n" + e);
+                    }
+                }
+
+            }
+        }
+
+        private List<Process> GetProcesses()
+        {
+            List<Process> procs = new List<Process>();
+            foreach (Process p in Process.GetProcesses())
+            {
+                try
+                {
+                    // Trycatch here to weed out non-accessable processes
+                    ProcessModule t = p.Modules[0];
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                procs.Add(p);
+            }
+            return procs;
+        }
+
+
+        #region " TCP Connections "
+        // Adapted from
+        // http://www.cheynewallace.com/get-active-ports-and-associated-process-names-in-c/
+        private List<Connection> GetConnections()
+        {
+            var Connections = new List<Connection>();
+
+            try
+            {
+                using (Process p = new Process())
+                {
+
+                    ProcessStartInfo ps = new ProcessStartInfo();
+                    ps.Arguments = "-a -n -o";
+                    ps.FileName = "netstat.exe";
+                    ps.UseShellExecute = false;
+                    ps.WindowStyle = ProcessWindowStyle.Hidden;
+                    ps.RedirectStandardInput = true;
+                    ps.RedirectStandardOutput = true;
+                    ps.RedirectStandardError = true;
+
+                    p.StartInfo = ps;
+                    p.Start();
+
+                    StreamReader stdOutput = p.StandardOutput;
+                    StreamReader stdError = p.StandardError;
+
+                    string content = stdOutput.ReadToEnd() + stdError.ReadToEnd();
+                    string exitStatus = p.ExitCode.ToString();
+
+                    if (exitStatus != "0")
+                    {
+                        Logger.LogError("Failed reading TCP connections.");
+                        return null;
+                    }
+
+                    string[] rows = Regex.Split(content, "\r\n");
+                    foreach (string row in rows)
+                    {
+                        if (String.IsNullOrEmpty(row)) continue;
+
+                        if (row.Contains("0.0.0.0") || row.Contains("127.0.0.1") || row.Contains("[::")) continue;
+                        string[] tokens = Regex.Split(row, "\\s+");
+                        if (tokens.Length > 4 && tokens[1].Equals("TCP"))
+                        {
+                            string t = tokens[3].Split(':')[1];
+                            int remotePort = Int32.Parse(t);
+                            Connections.Add(new Connection()
+                            {
+                                ProcessId = Int32.Parse(tokens[5]),
+                                RemotePort = remotePort,
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message);
+            }
+            return Connections;
+        }
+
+        public class Connection
+        {
+            public int RemotePort { get; set; }
+            public int ProcessId { get; set; }
+
+            public override string ToString()
+            {
+                return "TCP Connection - Process Id: " + ProcessId + ", Port: " + RemotePort;
+            }
+        }
+        #endregion
+
+        #region " Commandline Args "
+
+        private string GetCommandLine(Process process)
+        {
+            string cmdLine = null;
+            using (var searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId = " + process.Id))
+            {
+                var matchEnum = searcher.Get().GetEnumerator();
+                if (matchEnum.MoveNext())
+                {
+                    cmdLine = matchEnum.Current["CommandLine"]?.ToString();
+                }
+            }
+            return cmdLine;
+        }
+
+        #endregion
+    }
+}
--- MinerKiller/Program.cs
+++ MinerKiller/Program.cs
@@ -1,83 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 namespace MinerKiller
 {
     class Program
     {
-        private static int[] _PortList = new[]
-        {
-            9999,
-            14444,
-            14433,
-            6666,
-            16666,
-            6633,
-            16633,
-            4444,
-            14444,
-            3333,
-            13333,
-            7777,
-            5555
-        };
-
-        private static string[] _Nvidia = new[]
-        {
-            "nvcompiler.dll",
-            "nvopencl.dll",
-            "nvfatbinaryLoader.dll",
-            "nvapi64.dll",
-            "OpenCL.dll"
-        };
-
-        private static string[] _Amd = new[] { ""};
-        //todo: amd libraries
         static void Main(string[] args)
         {
             WaterMark();
-            
+
+            MinerKiller mk = new MinerKiller();
+
             Logger.Log("Scanning started...");
 
-            var cons = GetConnections();
+            mk.Scan();
             
-
-            foreach (Process p in Process.GetProcesses())
-            {
-                try
-                {
-                    ProcessModule t = p.Modules[0];
-                }
-                catch (Exception){
-                    continue;           
-                }
-
-                int modCount = 0;
-                foreach (ProcessModule pMod in p.Modules)
-                {
-                    foreach(string name in _Nvidia)
-                        if (pMod.ModuleName.ToLower().Equals(name.ToLower()))
-                            modCount++;   
-                }
-                if (modCount > 2)
-                {
-                    Logger.LogWarn("Miner Found - Process: " + p.ProcessName + ".exe, ProcessId: " + p.Id);
-                    var tiedConnections = cons.Where(x => x.ProcessId == p.Id);
-                    var badPorts = tiedConnections.Where(x => _PortList.Any(y => y == x.RemotePort));
-                    foreach (var conn in badPorts)
-                    {
-                        Logger.LogSuccess("\t" + conn);
-                    }
-                    p.Kill();
-                }
-            }
             Logger.Log("Finished.");
-            Console.Read();
 
+            Console.Read();
         }
 
         private static void WaterMark()
@@ -89,87 +28,6 @@ namespace MinerKiller
             Console.WriteLine(@"/_/  /_/_/_/ /_/\___/_/  /_/ |_/_/_/_/\___/_/     ");
             Console.WriteLine("\t\tby: Adam Roach\r\n");
         }
-        // Adapted from
-        // http://www.cheynewallace.com/get-active-ports-and-associated-process-names-in-c/
-        public static List<Connection> GetConnections()
-        {
-            var Connections = new List<Connection>();
 
-            try
-            {
-                using (Process p = new Process())
-                {
-
-                    ProcessStartInfo ps = new ProcessStartInfo();
-                    ps.Arguments = "-a -n -o";
-                    ps.FileName = "netstat.exe";
-                    ps.UseShellExecute = false;
-                    ps.WindowStyle = ProcessWindowStyle.Hidden;
-                    ps.RedirectStandardInput = true;
-                    ps.RedirectStandardOutput = true;
-                    ps.RedirectStandardError = true;
-
-                    p.StartInfo = ps;
-                    p.Start();
-
-                    StreamReader stdOutput = p.StandardOutput;
-                    StreamReader stdError = p.StandardError;
-
-                    string content = stdOutput.ReadToEnd() + stdError.ReadToEnd();
-                    string exitStatus = p.ExitCode.ToString();
-
-                    if (exitStatus != "0")
-                    {
-                        Logger.LogError("Failed reading TCP connections.");
-                        return null;
-                    }
-
-                    string[] rows = Regex.Split(content, "\r\n");
-                    foreach (string row in rows)
-                    {
-                        if (string.IsNullOrEmpty(row)) continue;
-
-                        if (row.Contains("0.0.0.0") || row.Contains("127.0.0.1") || row.Contains("[::")) continue;
-                        string[] tokens = Regex.Split(row, "\\s+");
-                        if (tokens.Length > 4 &&  tokens[1].Equals("TCP"))
-                        {
-                            string t = tokens[3].Split(':')[1];
-                            int remotePort = int.Parse(t);
-                            Connections.Add(new Connection()
-                            {
-                                ProcessId = int.Parse(tokens[5]),
-                                RemotePort = remotePort,
-                            });
-                        }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            return Connections;
-        }
-
-        public static string LookupProcess(int pid)
-        {
-            string procName;
-            try { procName = Process.GetProcessById(pid).ProcessName; }
-            catch (Exception) { procName = "-"; }
-            return procName;
-        }
-
-        public class Connection
-        {
-
-            public int RemotePort { get; set; }
-            public int ProcessId{ get; set; }
-
-            public override string ToString()
-            {
-                return "TCP Connection - Process Id: " + ProcessId + ", Port: " + RemotePort;
-            }
-
-        }
     }
 }
